8a8cbcb548a422d364b4609a56e50624642f9743
Ryker Sun <rykersun2002@gmail.com>
2023-10-17 23:28:41 +0800

    Add install_nvim.sh

---
+++ install_nvim.sh
@@ -0,0 +1,8 @@
+sudo apt install update
+sudo apt install curl -y
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+./nvim.appimage --appimage-extract
+sudo mv squashfs-root /
+sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
+rm nvim.appimage
