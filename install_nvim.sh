2fb1a4ee6073e39b649e909c8f134ac7f25d75aa
Ryker Sun <rykersun2002@gmail.com>
2024-04-13 00:16:49 +0800

    Update script

--- install_nvim.sh
+++ install_nvim.sh
@@ -1,8 +1,10 @@
-sudo apt install update
+sudo apt update
 sudo apt install curl -y
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
 ./nvim.appimage --appimage-extract
 sudo mv squashfs-root /
-sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
+sudo ln -s /squashfs-root/AppRun /usr/local/bin/nvim
+rm -rf squashfs-root
 rm nvim.appimage
+
